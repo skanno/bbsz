e226d178627fff180c7a55dc5ed7040096c82096
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-09-09 20:02:51 +0900

    マイグレーションを追加

---
+++ config/Migrations/20220909093424_CreateCategories.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class CreateCategories extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('categories');
+
+        $table->addColumn('name', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('level', 'integer', [
+            'default' => 1,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => 'CURRENT_TIMESTAMP',
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => 'CURRENT_TIMESTAMP',
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
---
+++ config/Migrations/20220909094956_CreateBoards.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class CreateBoards extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('boards');
+        $table->addColumn('category_id', 'integer', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('name', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('description', 'text', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => 'CURRENT_TIMESTAMP',
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => 'CURRENT_TIMESTAMP',
+            'null' => false,
+        ]);
+
+        $table->addForeignKey('category_id', 'categories', 'id', [
+            'delete' => 'cascade',
+            'update' => 'cascade',
+        ]);
+
+        $table->create();
+    }
+}
