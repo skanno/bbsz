a7f6162aedbb030e1907d1dbe7a2365d6636edf6
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-09-11 18:43:54 +0900

    ロケール関連の設定を変更。

--- config/app.php
+++ config/app.php
@@ -1,7 +1,6 @@
 <?php
 
 use Cake\Cache\Engine\FileEngine;
-use Cake\Cache\Engine\RedisEngine;
 use Cake\Database\Connection;
 use Cake\Database\Driver\Mysql;
 use Cake\Log\Engine\FileLog;
@@ -50,8 +49,8 @@ return [
     'App' => [
         'namespace' => 'App',
         'encoding' => env('APP_ENCODING', 'UTF-8'),
-        'defaultLocale' => env('APP_DEFAULT_LOCALE', 'en_US'),
-        'defaultTimezone' => env('APP_DEFAULT_TIMEZONE', 'UTC'),
+        'defaultLocale' => env('APP_DEFAULT_LOCALE', 'ja_JP'),
+        'defaultTimezone' => env('APP_DEFAULT_TIMEZONE', 'Asia/Tokyo'),
         'base' => false,
         'dir' => 'src',
         'webroot' => 'webroot',
