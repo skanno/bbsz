591e3742121fba4f314b778d3d1e43b261f8aa38
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-09-11 12:16:51 +0900

    不要なコードを削除。

--- templates/Boards/add.php
+++ templates/Boards/add.php
@@ -1,15 +1,7 @@
-<?php
-/**
- * @var \App\View\AppView $this
- * @var \App\Model\Entity\Board $board
- * @var \Cake\Collection\CollectionInterface|string[] $categories
- */
-?>
 <div class="row">
     <aside class="column">
         <div class="side-nav">
             <h4 class="heading">板を作成</h4>
-            <?= $this->Html->link(__('List Boards'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
         </div>
     </aside>
     <div class="column-responsive column-80">
