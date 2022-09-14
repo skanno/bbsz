cf2ef60bb2d7b8945c63f233efc8fcbc58c113bd
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-09-14 18:59:44 +0900

    Twitterの共有ボタンを実装。

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -40,6 +40,10 @@ if (count($crumbs)) {
     </header>
     <main class="main">
         <div class="container">
+            <div class="p-3">
+                <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a>
+                <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            <div>
             <?= $this->Breadcrumbs->render(
                 ['class' => 'breadcrumbs-trail'],
                 ['separator' => '<i class="fa fa-angle-right"></i>']
